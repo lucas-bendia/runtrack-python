58d3b84d6a6f3fc5bdd6594a093891aaab5b0c1a
lucas-bendia <lucas.bendia@laplateforme.io>
2023-11-27 16:35:38 +0100

    Job03 corrigé

--- Jour05/Job03/Job03.py
+++ Jour05/Job03/Job03.py
@@ -1,17 +1,7 @@
-# Définition de la fonction qui affiche le triangle
-def afficher_triangle(hauteur):
-    # Boucle sur chaque ligne du triangle
-    for i in range(hauteur):
-        # Calcul du nombre d'espaces nécessaires pour centrer le triangle
-        espaces = ' ' * (hauteur - i - 1)
-        # Construction de la ligne du triangle avec les caractères '/' et '\'
-        slashes = '/' + ' ' * (2 * i) + '\\'
-        # Affichage de la ligne du triangle
-        print(espaces + slashes)
-    # Affichage de la ligne du bas
-    print('_' * (2 * hauteur + 1))
+def triangle():
+    hauteur = int(input("hauteur : "))
+    for i in range(hauteur -1):
+        print(" " * (hauteur -i -1) + "/" + " " * (2*i) + "\\")
+    print("/" + "__" * (hauteur -1) + "\\")
 
-# Demande à l'utilisateur d'entrer la hauteur du triangle
-hauteur = int(input("Entrez la hauteur du triangle : "))
-# Appel de la fonction pour afficher le triangle
-afficher_triangle(hauteur)
+triangle()
